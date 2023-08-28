a360f26fdcd55e3a249f1adb7506227fbb8e59af
Nahid Hasan Kakon <nahid@nuarca.com>
2023-08-28 13:10:48 +0600

    Add project files.

---
+++ DependencyInversionPrinciple/Program.cs
@@ -0,0 +1,55 @@
+﻿public class Program
+{
+    static void Main()
+    {
+        INotification emailNotification = new EmailNotification();
+        INotification smsNotification = new SmsNotification();
+
+        NotificationService emailNotificationService = new NotificationService(emailNotification);
+        emailNotification.SendNotification("email sent");
+
+        NotificationService smsNotificationService = new NotificationService(smsNotification);
+        smsNotification.SendNotification("sms sent");
+    }
+   
+}
+
+
+// high level module
+public class NotificationService
+{
+    private INotification notification;
+    public NotificationService(INotification _notification) 
+    { 
+        notification = _notification;
+    }
+
+    public void Send(string message)
+    {
+        notification.SendNotification(message);
+    }
+}
+
+// abstraction
+public interface INotification
+{
+    public void SendNotification(string message);
+}
+
+
+// low level module
+public class EmailNotification : INotification
+{
+    public void SendNotification(string message)
+    {
+        Console.WriteLine(message);
+    }
+}
+
+public class SmsNotification : INotification
+{
+    public void SendNotification(string message)
+    {
+        Console.WriteLine(message);
+    }
+}
